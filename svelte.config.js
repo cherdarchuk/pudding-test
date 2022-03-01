9042e04cede2f1cd1bb68c461d6365e7ad450dab
Joey Cherdarchuk <joeycherdarchuk@gmail.com>
2022-03-01 14:03:02 -0700

    change adapter to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import adapterStatic from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import svg from "vite-plugin-svgstring";
 import dsv from "@rollup/plugin-dsv";
 import sveltePreprocess from "svelte-preprocess";
